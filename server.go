baccb83e04b422cb6349c03bb0142c36f5e1814e
Michal Pyszynski <michal.pyszynski@coop.no>
2021-03-10 15:31:02 +0100

    change docker tag

--- server.go
+++ server.go
@@ -1,32 +1,27 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"log"
 	"net/http"
-
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 	// Mongo
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://mongo-service:27017"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = client.Connect(context.TODO())
-	if err != nil {
-		log.Fatal(err)
-	}
+	// client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://mongo-service:27017"))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// err = client.Connect(context.TODO())
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		err := client.Ping(context.TODO(), nil)
-		if err != nil {
-			log.Fatal(err)
-		}
+		// err := client.Ping(context.TODO(), nil)
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
 		w.Write([]byte(fmt.Sprintf("Hello World")))
 	})
 
