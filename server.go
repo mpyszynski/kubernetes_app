63aec938c33d37da1e049a3562c5edf0cfbf0cc7
Michal Pyszynski <michal.pyszynski@coop.no>
2021-03-10 14:01:08 +0100

    Initial

---
+++ server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	// Mongo
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://mongo-service:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Connect(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		err := client.Ping(context.TODO(), nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		w.Write([]byte(fmt.Sprintf("Hello World")))
+	})
+
+	server := http.Server{
+		Addr:    ":8000",
+		Handler: mux,
+	}
+
+	fmt.Println("Server is listening on port :8000")
+	server.ListenAndServe()
+}
